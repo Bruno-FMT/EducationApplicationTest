51694a5b99e303d472a0230e7b622d3d9ff9d42a
CorreaBrunoMiguel <devsistemascorrea@gmail.com>
2024-02-28 19:11:36 -0300

    Criado Projeto Spring

---
+++ src/main/java/br/com/fullstack/eduation/EduationApplication.java
@@ -0,0 +1,16 @@
+package br.com.fullstack.eduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduationApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EduationApplication.class, args);
+
+	}
+
+}
